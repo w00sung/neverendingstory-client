64886535c30945956da29caf407beb40a59d8205
stkang9409 <stkang9409@gmail.com>
2021-03-26 16:12:46 +0000

    asdf

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://15.164.48.192:5000',
+            target: 'http://15.164.48.192:80',
             changeOrigin: true,
         })
     );

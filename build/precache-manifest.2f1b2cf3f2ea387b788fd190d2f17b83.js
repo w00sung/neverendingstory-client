7b921164a556b94172815a733d2770d90184efec
w00sung <jeongws3240@gmail.com>
2021-03-28 19:30:07 +0900

    ADD build

---
+++ build/precache-manifest.2f1b2cf3f2ea387b788fd190d2f17b83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9444f12120f83e09c10c719be73a9e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b4f26e87b9b807203e",
+    "url": "/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "94b80d774fe323193657",
+    "url": "/static/css/main.1a7127ff.chunk.css"
+  },
+  {
+    "revision": "53b4f26e87b9b807203e",
+    "url": "/static/js/2.3cd7fc55.chunk.js"
+  },
+  {
+    "revision": "aa304a03a8d97b7d7180ac18030a3807",
+    "url": "/static/js/2.3cd7fc55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b80d774fe323193657",
+    "url": "/static/js/main.2e118b20.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
